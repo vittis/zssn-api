a3fcd68b49029b0c4a63143b3b8fb53c84421430
vittis <vitbolb1@gmail.com>
2019-12-18 10:52:45 -0300

    Add permissive headers

--- src/app.ts
+++ src/app.ts
@@ -23,6 +23,15 @@ DbConnection.initConnection().then(() => {
       }),
     );
     app.use(bodyParser.json());
+    app.use(function(req, res, next) {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+      );
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+      next();
+    });
   });
 
   const serverInstance = server.build();
